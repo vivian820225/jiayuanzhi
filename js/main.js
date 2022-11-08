779fadd453acd3ad12529fe8bfc32c3b61e88230
Vivian.C <vivian820225@gmail.com>
2022-11-08 23:26:24 +0800

    [Fix] 依需求調整

--- js/main.js
+++ js/main.js
@@ -1,93 +1,72 @@
-let timer = null
-let callback = ([entry]) => {
-    const toast = document.querySelector('.toast')
-    if (entry.isIntersecting) {
-      toast.classList.add('active')
-      timer = setTimeout(() => {
-        toast.classList.remove('active')
-       }, 5 * 1000);
-      return
-    }
-    clearTimeout(timer)
-    toast.classList.remove('active')
-}
-
-const options = { threshold: 0 }
-
-const observer = new IntersectionObserver(callback, options)
-
-const section = document.querySelector('.sec4')
-observer.observe(section)
-
-function initPositionImg() {
-  const dragImg = document.querySelector('.scroll-img')
-  const positionLeft = dragImg.clientWidth / 2 - dragContainer.clientWidth / 2
-  dragContainer.scrollLeft = positionLeft
-}
-
-const ImgObserver = new ResizeObserver(([entry]) => {
-  if (entry && entry.target) {
-    initPositionImg()
-  }
+AOS.init();
+const swiper2 = new Swiper('#sec2-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  
+  pagination: {
+    el: "#sec2-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#sec2-swiper .swiper-button-next',
+    prevEl: '#sec2-swiper .swiper-button-prev',
+  },
+  breakpoints: {},
 });
 
-const dragContainer = document.querySelector('.horizontal-scroll-container')
-ImgObserver.observe(dragContainer, {box: "border-box"});
-
-$(document).ready(function () {
-  AOS.init();
+const swiper3 = new Swiper('#sec3-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   
-  const swiper1 = new Swiper('#sec2-swiper', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    
-    pagination: {
-      el: "#sec2-swiper .swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '#sec2-swiper .swiper-button-next',
-      prevEl: '#sec2-swiper .swiper-button-prev',
-    },
-    breakpoints: {},
-  });
+  pagination: {
+    el: "#sec3-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#sec3-swiper .swiper-button-next',
+    prevEl: '#sec3-swiper .swiper-button-prev',
+  },
+  breakpoints: {},
+});
 
-  const swiper2 = new Swiper('#sec3-swiper', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    
-    pagination: {
-      el: "#sec3-swiper .swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '#sec3-swiper .swiper-button-next',
-      prevEl: '#sec3-swiper .swiper-button-prev',
-    },
-    breakpoints: {},
-  });
+const swiper6 = new Swiper('#sec6-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  
+  pagination: {
+    el: "#sec6-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#sec6-swiper .swiper-button-next',
+    prevEl: '#sec6-swiper .swiper-button-prev',
+  },
+  breakpoints: {},
+});
 
-  const swiper3 = new Swiper('#sec7-swiper', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    
-    pagination: {
-      el: "#sec7-swiper .swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '#sec7-swiper .swiper-button-next',
-      prevEl: '#sec7-swiper .swiper-button-prev',
-    },
-    breakpoints: {},
-  });
+const swiper7 = new Swiper('#sec7-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  
+  pagination: {
+    el: "#sec7-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#sec7-swiper .swiper-button-next',
+    prevEl: '#sec7-swiper .swiper-button-prev',
+  },
+  breakpoints: {},
 });
